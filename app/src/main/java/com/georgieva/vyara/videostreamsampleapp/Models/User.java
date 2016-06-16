1f75a3a32cfe48c0502ff18bf269ebede52e4d7a
VyaraGGeorgieva <viara.georgieva.g@gmail.com>
2016-06-16 18:32:03 +0300

    user's fields modified

--- app/src/main/java/com/georgieva/vyara/videostreamsampleapp/Models/User.java
+++ app/src/main/java/com/georgieva/vyara/videostreamsampleapp/Models/User.java
@@ -6,19 +6,10 @@ package com.georgieva.vyara.videostreamsampleapp.Models;
 public class User {
 
     //variables in the JSON
-    private String name;
     private String password;
-
+    private String email;
 
     //getter-setter pairs
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -26,4 +17,13 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
