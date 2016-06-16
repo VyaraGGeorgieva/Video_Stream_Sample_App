4293d1854b00042b95a8afbdac7fbedd8509310f
VyaraGGeorgieva <viara.georgieva.g@gmail.com>
2016-06-16 18:31:25 +0300

    Test Mocky (GET)

--- app/src/main/java/com/georgieva/vyara/videostreamsampleapp/Activities/LoginActivity.java
+++ app/src/main/java/com/georgieva/vyara/videostreamsampleapp/Activities/LoginActivity.java
@@ -3,26 +3,80 @@ package com.georgieva.vyara.videostreamsampleapp.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.georgieva.vyara.videostreamsampleapp.Models.User;
 import com.georgieva.vyara.videostreamsampleapp.R;
+import com.georgieva.vyara.videostreamsampleapp.RetrofitObjectAPI;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class LoginActivity extends AppCompatActivity {
 
     Intent i;
+    String url = "http://www.mocky.io/";
+    EditText input_email;
+    EditText input_pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        input_email = (EditText) findViewById(R.id.input_email);
+        input_pass = (EditText) findViewById(R.id.input_pass);
     }
 
-    public void register(View view){
+    public void register(View view) {
         i = new Intent(view.getContext(), RegisterActivity.class);
         startActivity(i);
     }
 
-    public void playMusic(View view){
-        i = new Intent(view.getContext(), LivestreamVideoActivity.class);
-        startActivity(i);
+    public void playMusic(View view) {
+//        i = new Intent(view.getContext(), LivestreamVideoActivity.class);
+//        startActivity(i);
+        getUser();
+    }
+
+    private void getUser() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitObjectAPI service = retrofit.create(RetrofitObjectAPI.class);
+
+        Call<User> call = service.getUserDetails();
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Response<User> response, Retrofit retrofit) {
+
+                try {
+
+                    input_email.setText(response.body().getEmail());
+                    input_pass.setText(response.body().getPassword());
+
+
+                } catch (Exception e) {
+                    Log.d("onResponse", "There is an error");
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("onFailure", t.toString());
+            }
+
+
+        });
     }
 }
