87fcfdee7ec84eeb36a7496eb7614327d4317089
VyaraGGeorgieva <viara.georgieva.g@gmail.com>
2016-06-18 16:02:59 +0300

    var's initializations moved

--- app/src/main/java/com/georgieva/vyara/videostreamsampleapp/Activities/LivestreamVideoActivity.java
+++ app/src/main/java/com/georgieva/vyara/videostreamsampleapp/Activities/LivestreamVideoActivity.java
@@ -12,26 +12,22 @@ import com.georgieva.vyara.videostreamsampleapp.R;
 
 public class LivestreamVideoActivity extends AppCompatActivity {
 
-    private int duration;
+    private String path = "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8";
+    private VideoView videoView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_livestream_video);
 
-        VideoView videoView = (VideoView) findViewById(R.id.videoView);
-        String httpLiveUrl = "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8";
-
-        videoView.setVideoURI(Uri.parse(httpLiveUrl));
+        videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse(path));
         videoView.setMediaController(new MediaController(this));
 
         videoView.requestFocus();
         videoView.start();
         unmute();
-    }
-
-    public void mute() {
-        AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        am.setStreamMute(AudioManager.STREAM_MUSIC, true);
     }
 
     public void unmute() {
